502278f44862564a39e8da576517106b9a1ccf64
Matthew Camp <mdcamp00@gmail.com>
2018-02-14 23:14:59 -0800

    Completed Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public static GameManager instance = null;
     public BoardManager boardManager;
 
     private int level = 3;
 
     void Awake()
     {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
         boardManager = GetComponent<BoardManager>();
         InitGame();
     }
